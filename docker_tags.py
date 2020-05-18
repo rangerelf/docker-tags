3224d1affec53fbe3f3488f1d38291f8b42f8d08
Gustavo Cordova <gustavo.cordova@gmail.com>
2020-05-18 08:03:56 -0700

    Remove templating, use report class

--- docker_tags.py
+++ docker_tags.py
@@ -22,62 +22,86 @@ def hrn(num, magnitude=1024):
 class BadResponseStatus(ValueError):
     "Request returned a bad response status."
 
-def get(url, log_json=None):
+def get(url, json_log=None):
     "Iterate the data pages from the given url"
     while url:
         rsp = urllib.request.urlopen(url)
         if not 200 <= rsp.getcode() < 300:
             raise BadResponseStatus(rsp.getcode())
         text = rsp.read()
-        if log_json:
-            log_json.write(text.decode('utf8'))
+        if json_log:
+            json_log.write(text.decode('utf8'))
         data = json.loads(text)
         yield data
         url = data['next']
 
 def repo_url(name, registry=DOCKER_HUB_REGISTRY):
     "Yield each url created from the repo names in 'names'"
-    prefix = f"{registry}/v2/repositories"
     if '/' in name:
-        return f"{prefix}/{name}/tags/"
-    return f"{prefix}/library/{name}/tags/"
+        return f"{registry}/v2/repositories/{name}/tags/"
+    return f"{registry}/v2/repositories/library/{name}/tags/"
 
-def compile_template(template):
-    "Return a code object to run the template"
-    prefix, template = template.split(":", 1)
-    if prefix == 'D':
-        co_o = compile(f'''f"{template}"''', '<template>', 'eval')
-        def template_func(page, _vars):
-            "Run the template code in the context of the page"
-            return eval(co_o, {**globals(), **_vars}, page)
-    elif prefix == 'L':
-        co_o = compile(f'''(f"{template}")''', '<template>', 'eval')
-        def template_func(page, _vars):
-            "Run the template code in the context of the result line"
-            base_ns = {**globals(), **_vars}
-            return "\n".join(eval(co_o, {**base_ns, **_})
-                             for _ in page["results"])
-    else:
-        raise ValueError(f"Unknown template prefix: {prefix}")
-    return template_func
-
-def run_report(docker_repos, **kwarg):
-    "Report versions of images found in 'docker_repositories'"
+def brief_report(docker_repos, **kwarg):
+    "One line per docker tag version"
     jsl = kwarg.get('json_log')
-    tmpl_f = compile_template(kwarg.get('template'))
-    try:
-        for repository_name in docker_repos:
-            for page in get(repo_url(repository_name), log_json=jsl):
-                report_page = tmpl_f(page, locals())
-                print(report_page)
-    except KeyboardInterrupt:
-        print("")
+    for nth, repo_name in enumerate(docker_repos):
+        if nth:
+            print("="*64)
+        for page in get(repo_url(repo_name), json_log=jsl):
+            version = page["name"]
+            size = hrn(page["full_size"])
+            arch = ", ".join(_["arch"] for _ in page["images"])
+            print(f"{repo_name}:{version} Size: {size} Arch: {arch}")
 
-# Dictionary of callables with the format.
-TEMPLATES = {
-    'short': "L:{repository_name}:{name} ({hrn(full_size)})",
-    'raw': "D:json.dumps(page, indent=2)"
-}
+def report(report_obj, docker_repo, json_log):
+    "Send out the report"
+    for nth, repo_name in enumerate(docker_repos):
+        if nth:
+            report_obj.separator()
+        for page in get(repo_url(repo_name), json_log=jsl):
+            report_obj.page(repo_name, page)
+
+class Report:
+    def __init__(self, stream):
+        self._stream = stream 
+    def report(self, docker_repos, json_log):
+        "The main report loop"
+        repo_num = 0 # Have to pre-initialize repo_num ...
+        for repo_num, repo_name in enumerate(docker_repos):
+            if repo_num:
+                self.separator()
+            url = repo_url(repo_name)
+            for page_num, page in enumerate(url, json_log=json_log):
+                self.report_page(repo_num, repo_name, page_num, page)
+        # because if docker_repos is empty then repo_num will be undefined
+        if repo_num:
+            self.finish_report()
+
+    def report_page(self, repo_num, repo_name, page_num, page_data):
+        "Print out the page data"
+        raise NotImplementedError()
+
+    def separator(self):
+        "Print out a separator line between repositories"
+        raise NotImplementedError()
+
+class RawReport(Report):
+    "Print out the raw json"
+    def separator(self):
+        self.stream.write(",\n")
+    def report_page(self, repo_num, repo_name, page_num, page_data):
+        self.stream.write(json.dumps(page_data, indent=2))
+
+class BriefReport(Report):
+    "Print out a per-line report"
+    def separator(self):
+        self.stream.write(f"{'='*64}\n")
+
+    def report_page(self, repo_num, repo_name, page_num, page_data):
+        for ent in page_data["entries"]:
+            version = ent["name"]
+            size = hrn(ent["full_size"])
+            self.stream.write(f"{repo_name}:{version} ({size})\n")
 
 def main():
     "Run from the command line"
